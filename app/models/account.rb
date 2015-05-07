9149dea690ca5f56eb7158dfae55057f60259290
Yann Lugrin <yann.lugrin@yalty.io>
2015-05-07 10:38:54 +0200

    validate format of subdomain

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,6 @@
 class Account < ActiveRecord::Base
-  validates :subdomain, presence: true, uniqueness: { case_sensitive: false }
+  validates :subdomain, presence: true,
+                        uniqueness: { case_sensitive: false },
+                        format: { with: /\A[0-9a-z][0-9a-z\-]+[0-9a-z]\z/, allow_blank: true }
   validates :company_name, presence: true
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe Account, type: :model do
   it { should have_db_index(:subdomain).unique(true) }
   it { should validate_presence_of(:subdomain) }
   it { should validate_uniqueness_of(:subdomain).case_insensitive }
+  it { should allow_value('subdomain', 'sub-domain', '123subdomain', 'subdomain-123').for(:subdomain) }
+  it { should_not allow_value('-subdomain', 'subdomain-', 'sub domain', 'subdömaìn', 'SubDomain').for(:subdomain) }
 
   it { should have_db_column(:company_name) }
   it { should validate_presence_of(:company_name) }
