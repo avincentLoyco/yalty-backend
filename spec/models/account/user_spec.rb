532c435986a9bcb885acdab47c9c2a4bfc0adf3d
Yann Lugrin <yann.lugrin@yalty.io>
2015-05-12 16:59:43 +0200

    Validate length of password only on change

--- spec/models/account/user_spec.rb
+++ spec/models/account/user_spec.rb
@@ -10,6 +10,13 @@ RSpec.describe Account::User, type: :model do
   it { should validate_confirmation_of(:password) }
   it { should validate_length_of(:password).is_at_least(8).is_at_most(74) }
 
+  it 'should validate length of password only when is updated' do
+    user = FactoryGirl.create(:account_user)
+    user = Account::User.find(user.id)
+
+    expect(user).to be_valid
+  end
+
   it { should have_db_column(:account_id) }
   it { should have_db_index(:account_id) }
   it { should belong_to(:account).inverse_of(:users) }
