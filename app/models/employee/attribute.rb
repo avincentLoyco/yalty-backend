ba503cbbad41fdbbe691eef556dcca87ad9d89ee
Yann Lugrin <yann.lugrin@yalty.io>
2015-06-22 11:48:58 +0200

    attribute has one account through employee

--- app/models/employee/attribute.rb
+++ app/models/employee/attribute.rb
@@ -2,4 +2,5 @@ class Employee::Attribute < ActiveRecord::Base
   include AttributeSerializer
 
   belongs_to :employee, inverse_of: :employee_attributes
+  has_one :account, through: :employee
 end
--- spec/models/employee/attribute_spec.rb
+++ spec/models/employee/attribute_spec.rb
@@ -10,4 +10,6 @@ RSpec.describe Employee::Attribute, type: :model do
   it { is_expected.to have_db_column(:employee_id) }
   it { is_expected.to have_db_index(:employee_id) }
   it { is_expected.to belong_to(:employee).inverse_of(:employee_attributes) }
+
+  it { is_expected.to have_one(:account).through(:employee) }
 end
