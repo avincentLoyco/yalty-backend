807fd47afcbb7c5f3f4e266a5bf860d41fa962e8
Yann Lugrin <yann.lugrin@yalty.io>
2015-08-18 16:26:06 +0200

    fix EmployeeAttribute namespace

--- app/resources/api/v1/employee_attribute_resource.rb
+++ app/resources/api/v1/employee_attribute_resource.rb
@@ -1,6 +1,7 @@
 module API
   module V1
     class EmployeeAttributeResource < JSONAPI::Resource
+      model_name 'Employee::Attribute'
       attributes :name, :attribute_type, :data
     end
   end
--- app/resources/api/v1/employee_resource.rb
+++ app/resources/api/v1/employee_resource.rb
@@ -1,7 +1,7 @@
 module API
   module V1
     class EmployeeResource < JSONAPI::Resource
-      has_many :employee_attributes
+      has_many :employee_attributes, class_name: 'EmployeeAttribute'
 
       def self.records(options = {})
         Account.current.employees
