c184781b4c3ee46a199ae3a6c8b8e5ad0e55985f
Yann Lugrin <yann.lugrin@yalty.io>
2015-09-08 09:58:43 +0200

    test Employee::Attribute

--- app/models/employee/attribute.rb
+++ app/models/employee/attribute.rb
@@ -1,6 +1,15 @@
 class Employee::Attribute < ActiveRecord::Base
   self.primary_key = 'id'
 
+  belongs_to :employee, inverse_of: :employee_attributes
+  belongs_to :attribute_definition,
+    ->(attr) { readonly },
+    class_name: 'Employee::AttributeDefinition'
+  belongs_to :event,
+    class_name: 'Employee::Event',
+    foreign_key: 'employee_event_id'
+  has_one :account, through: :employee
+
   private
 
   def readonly?
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -18,5 +18,7 @@ RSpec.describe Employee, type: :model do
 
   it { is_expected.to have_many(:employee_attribute_versions).inverse_of(:employee) }
 
+  it { is_expected.to have_many(:employee_attributes).inverse_of(:employee) }
+
   it { is_expected.to have_many(:events).inverse_of(:employee) }
 end
