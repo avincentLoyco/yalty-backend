bd75a1121fc895334e7d13a893037aa7955ae56c
Yann Lugrin <yann.lugrin@yalty.io>
2015-09-08 14:55:54 +0200

    fix attribute json data

--- app/resources/api/v1/employee_attribute_resource.rb
+++ app/resources/api/v1/employee_attribute_resource.rb
@@ -2,7 +2,7 @@ module API
   module V1
     class EmployeeAttributeResource < JSONAPI::Resource
       model_name 'Employee::Attribute'
-      attributes :name, :attribute_type, :data
+      attributes :attribute_name, :attribute_type, :data, :effective_at
     end
   end
 end
