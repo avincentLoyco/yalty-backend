0943c109360abb231e04967af0743ccd276e094f
Yann Lugrin <yann.lugrin@yalty.io>
2015-09-11 12:00:11 +0200

    API to save new employee (first pass)
    Employee and event is saved, but attributes not yet.
    
    YWA-25

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -1,6 +1,83 @@
+require 'jsonapi/exceptions'
+
+module JSONAPI
+  module Exceptions
+    class EntityAlreadyExists < Error
+      def initialize(id)
+        @id = id
+      end
+
+      def errors
+        [
+          JSONAPI::Error.new(
+            code: JSONAPI::SAVE_FAILED,
+            status: :conflict,
+            title: 'Entity already exists',
+            detail: "Entity with id '#{@id} already exists'"
+          )
+        ]
+      end
+    end
+  end
+end
+
 module API
   module V1
     class EmployeesController < JSONAPI::ResourceController
+      skip_before_filter :setup_request, only: [:create]
+
+      def create
+        params.deep_transform_keys! {|key| unformat_key(key) }
+
+        # Validate employee data
+        employee_data = params.require(:data)
+
+        verify_type(employee_data[:type], EmployeeResource)
+        if Employee.where(id: employee_data[:id]).exists?
+          fail JSONAPI::Exceptions::EntityAlreadyExists.new(employee_data[:id])
+        end
+
+        # Validate and extract event data
+        event_data = employee_data.require(:relationships).require(:events).require(:data)
+
+        if !event_data.is_a?(Array) || event_data.size != 1
+          fail JSONAP::Exceptions::InvalidLinksObject.new
+        end
+
+        verify_type(event_data.first[:type], EmployeeEventResource)
+
+        event_attributes = event_data.first.require(:attributes).permit('effective_at')
+        event_attributes[:id] = event_data.first[:id]
+
+        # Create employee
+        employee = Account.current.employees.build(id: employee_data[:id])
+        event = employee.events.build(event_attributes)
+
+        if employee.save
+          render status: :created, nothing: true
+        else
+          fail JSONAPI::Exceptions::SaveFailed.new
+        end
+      rescue ActionController::ParameterMissing => e
+        render_errors(JSONAPI::Exceptions::ParameterMissing.new(e.param).errors)
+      rescue => e
+        handle_exceptions(e)
+      end
+
+      private
+
+      def verify_type(type, resource_klass)
+        if type.nil?
+          fail JSONAPI::Exceptions::ParameterMissing.new(:type)
+        elsif unformat_key(type).to_sym != resource_klass._type
+          fail JSONAPI::Exceptions::InvalidResource.new(type)
+        end
+      end
+
+      def unformat_key(key)
+        unformatted_key = key_formatter.unformat(key)
+        unformatted_key.nil? ? nil : unformatted_key.to_sym
+      end
     end
   end
 end
---
+++ spec/controllers/api/v1/employees_controller_spec.rb
@@ -0,0 +1,108 @@
+require 'rails_helper'
+
+RSpec.describe API::V1::EmployeesController, type: :controller do
+  let(:account) { user.account }
+  let(:user) { FactoryGirl.create(:account_user) }
+
+  let(:attribute_definition) {
+    FactoryGirl.create(
+      :employee_attribute_definition,
+      attribute_type: 'String',
+      account: account
+    )
+  }
+
+  let(:employee_uuid) { SecureRandom.uuid }
+  let(:event_uuid) { SecureRandom.uuid }
+  let(:attribute_uuid) { SecureRandom.uuid }
+
+  let(:json_payload) {
+    {
+      'data'=> {
+        'type' => 'employees',
+        'id' => employee_uuid,
+        'relationships' => {
+          'events' => {
+            'data' => [
+              {
+                'type' => 'employee-events',
+                'id' => event_uuid,
+                'attributes' => {
+                  'event-type' => 'hired',
+                  'effective-at' => '2015-09-10T00:00:00.000Z'
+                },
+                'relationships' => {
+                  'employee-attributes': {
+                    data: [
+                      {
+                        'type' => 'employee-attributes',
+                        'id' => attribute_uuid,
+                        'attributes' => {
+                          'value' => 'Fred',
+                        },
+                        'relationships' => {
+                          'attribute-definition' => {
+                            'type' => 'employee-attribute-definitions',
+                            'id' => "#{attribute_definition.id}"
+                          }
+                        }
+                      }
+                    ]
+                  }
+                }
+              }
+            ]
+          }
+        }
+      }
+    }
+  }
+
+  before(:each) do
+    Account.current = account
+    @request.headers.merge!(
+      'CONTENT-TYPE' => 'application/vnd.api+json',
+      'ACCEPT' => 'application/vnd.api+json'
+    )
+  end
+
+  context 'POST #create' do
+
+    it 'should return bad request status if data is not present' do
+      post :create, {}
+
+      expect(response).to have_http_status(:bad_request)
+    end
+
+    it 'should return bad request status if type is not employees' do
+      post :create, { 'data' => { 'type' => 'badtypes' }}
+
+      expect(response).to have_http_status(:bad_request)
+    end
+
+    it 'should return conflict status if employee already exists' do
+      FactoryGirl.create(:employee, id: employee_uuid, account: account)
+
+      post :create, json_payload
+
+      expect(response).to have_http_status(:conflict)
+    end
+
+    it 'create an employee with given uuid' do
+      expect {
+        post :create, json_payload
+      }.to change(account.employees.where(id: employee_uuid), :count).by(1)
+
+      expect(response).to have_http_status(:created)
+    end
+
+    it 'create an event with given uuid' do
+      expect {
+        post :create, json_payload
+      }.to change(Employee::Event.where(id: event_uuid), :count).by(1)
+
+      expect(response).to have_http_status(:created)
+    end
+
+  end
+end
