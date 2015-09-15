80d2c167bed70aef9b2d5ad29f06ebfc5b552754
Yann Lugrin <yann.lugrin@yalty.io>
2015-09-15 17:26:37 +0200

    Extract employee creation from employees controller.
    YWA-25

---
+++ app/controllers/api/v1/employee_management.rb
@@ -0,0 +1,122 @@
+require 'active_support/concern'
+require 'jsonapi/exceptions'
+
+module JSONAPI
+  module Exceptions
+    class EntityAlreadyExists < Error
+      def initialize(id)
+        @id = id
+      end
+
+      def errors
+        [
+          JSONAPI::Error.new(
+            code: JSONAPI::SAVE_FAILED,
+            status: :conflict,
+            title: 'Entity already exists',
+            detail: "Entity with id '#{@id} already exists'"
+          )
+        ]
+      end
+    end
+  end
+end
+
+module API
+  module V1
+    module EmployeeManagement
+      extend ActiveSupport::Concern
+
+      private
+
+      def setup_employee_management
+        params.deep_transform_keys! {|key| unformat_key(key) }
+      end
+
+      def build_employee(data)
+        verify_type(data[:type], EmployeeResource)
+        verify_entity_uniqueness(data[:id], Employee)
+
+        @employee = Account.current.employees.build(id: data[:id])
+      end
+
+      def build_employee_event(data)
+        if !data.is_a?(Array) || data.size != 1
+          fail JSONAP::Exceptions::InvalidLinksObject.new
+        end
+
+        data = data.first
+
+        verify_type(data[:type], EmployeeEventResource)
+        verify_entity_uniqueness(data[:id], Employee::Event)
+
+        event_attributes = data.require(:attributes).permit(:effective_at)
+        event_attributes[:id] = data[:id]
+
+        @event = @employee.events.build(event_attributes)
+      end
+
+      def build_employee_attributes(data)
+        if !data.is_a?(Array) || data.size < 1
+          fail JSONAP::Exceptions::InvalidLinksObject.new
+        end
+
+        attributes = []
+        data.each do |attr|
+          verify_type(attr[:type], EmployeeAttributeResource)
+          verify_entity_uniqueness(attr[:id], Employee::AttributeVersion)
+
+          attribute_definition = Account.current.employee_attribute_definitions
+          .where(
+            id: attr.require(:relationships).require(:attribute_definition)[:id]
+          ).first
+
+          attributes << {
+            data: attr.require(:attributes),
+            id: attr[:id],
+            event: @event,
+            attribute_definition: attribute_definition
+          }
+        end
+
+        @employee.employee_attribute_versions.build(attributes)
+      end
+
+      def save_employee
+        if @employee.save
+          render status: :created, nothing: true
+        else
+          fail JSONAPI::Exceptions::SaveFailed.new
+        end
+      end
+
+      def verify_type(type, resource_klass)
+        if type.nil?
+          fail JSONAPI::Exceptions::ParameterMissing.new(:type)
+        elsif unformat_key(type).to_sym != resource_klass._type
+          fail JSONAPI::Exceptions::InvalidResource.new(type)
+        end
+      end
+
+      def verify_entity_uniqueness(id, entity_klass)
+        if entity_klass.where(id: id).exists?
+          fail JSONAPI::Exceptions::EntityAlreadyExists.new(id)
+        end
+      end
+
+      def unformat_key(key)
+        unformatted_key = key_formatter.unformat(key)
+        unformatted_key.nil? ? nil : unformatted_key.to_sym
+      end
+
+      def handle_exceptions(e)
+        case e
+        when ActionController::ParameterMissing
+          render_errors(JSONAPI::Exceptions::ParameterMissing.new(e.param).errors)
+        else
+          super(e)
+        end
+      end
+    end
+  end
+end
--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -1,109 +1,37 @@
-require 'jsonapi/exceptions'
-
-module JSONAPI
-  module Exceptions
-    class EntityAlreadyExists < Error
-      def initialize(id)
-        @id = id
-      end
-
-      def errors
-        [
-          JSONAPI::Error.new(
-            code: JSONAPI::SAVE_FAILED,
-            status: :conflict,
-            title: 'Entity already exists',
-            detail: "Entity with id '#{@id} already exists'"
-          )
-        ]
-      end
-    end
-  end
-end
-
 module API
   module V1
     class EmployeesController < JSONAPI::ResourceController
+      include API::V1::EmployeeManagement
+
       skip_before_filter :setup_request, only: [:create]
 
       def create
-        params.deep_transform_keys! {|key| unformat_key(key) }
-
-        # Build Employee
-        employee_data = params.require(:data)
-
-        verify_type(employee_data[:type], EmployeeResource)
-        if Employee.where(id: employee_data[:id]).exists?
-          fail JSONAPI::Exceptions::EntityAlreadyExists.new(employee_data[:id])
-        end
-
-        employee = Account.current.employees.build(id: employee_data[:id])
-
-        # Build Employee::Event
-        event_data = employee_data.require(:relationships).require(:events).require(:data)
-
-        if !event_data.is_a?(Array) || event_data.size != 1
-          fail JSONAP::Exceptions::InvalidLinksObject.new
-        end
-
-        verify_type(event_data.first[:type], EmployeeEventResource)
-
-        event_attributes = event_data.first.require(:attributes).permit(:effective_at)
-        event_attributes[:id] = event_data.first[:id]
-
-        event = employee.events.build(event_attributes)
-
-        # Build Employee::AttributeVersion
-        attribute_data = event_data.first.require(:relationships).require(:employee_attributes).require(:data)
-
-        if !event_data.is_a?(Array) || event_data.size < 1
-          fail JSONAP::Exceptions::InvalidLinksObject.new
-        end
-
-        attributes = []
-        attribute_data.each do |data|
-          verify_type(data[:type], EmployeeAttributeResource)
-
-          attribute_definition = Account.current.employee_attribute_definitions
-          .where(
-            id: data.require(:relationships).require(:attribute_definition)[:id]
-          ).first
-
-          attributes << {
-            data: data.require(:attributes),
-            id: data[:id],
-            event: event,
-            attribute_definition: attribute_definition
-          }
-        end
-
-        employee.employee_attribute_versions.build(attributes)
-
-        # Save new Employee
-        if employee.save
-          render status: :created, nothing: true
-        else
-          fail JSONAPI::Exceptions::SaveFailed.new
-        end
-      rescue ActionController::ParameterMissing => e
-        render_errors(JSONAPI::Exceptions::ParameterMissing.new(e.param).errors)
+        setup_employee_management
+        build_employee(employee_data)
+        build_employee_event(employee_event_data)
+        build_employee_attributes(employee_attribute_data)
+        save_employee
       rescue => e
         handle_exceptions(e)
       end
 
       private
 
-      def verify_type(type, resource_klass)
-        if type.nil?
-          fail JSONAPI::Exceptions::ParameterMissing.new(:type)
-        elsif unformat_key(type).to_sym != resource_klass._type
-          fail JSONAPI::Exceptions::InvalidResource.new(type)
-        end
+      def employee_data
+        params.require(:data)
       end
 
-      def unformat_key(key)
-        unformatted_key = key_formatter.unformat(key)
-        unformatted_key.nil? ? nil : unformatted_key.to_sym
+      def employee_event_data
+        employee_data.require(:relationships)
+          .require(:events)
+          .require(:data)
+      end
+
+      def employee_attribute_data
+        employee_event_data.first
+          .require(:relationships)
+          .require(:employee_attributes)
+          .require(:data)
       end
     end
   end
