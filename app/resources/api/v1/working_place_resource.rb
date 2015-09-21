992fe1c8f4c64a5dec1b28f19c5109b26541988e
Jakub Cieslar <cieslar.jakub@gmail.com>
2015-09-21 16:51:29 +0200

    [YWA-38] Fix typo and add factories for working_places

--- app/resources/api/v1/working_place_resource.rb
+++ app/resources/api/v1/working_place_resource.rb
@@ -3,7 +3,7 @@ module API
     class WorkingPlaceResource < JSONAPI::Resource
 
       def self.records(options = {})
-        Account.current.workking_places
+        Account.current.working_places
       end
     end
   end
