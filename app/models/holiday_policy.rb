cc91b03929fe4e986f01e4d4dbbd855bfb060aa2
Magdalena Poprawa <magdalena.poprawa@monterail.com>
2015-09-25 17:24:59 +0200

    [YWA-49] Add validation for region
    Work in progress, refactor needed.

--- app/models/holiday_policy.rb
+++ app/models/holiday_policy.rb
@@ -2,18 +2,33 @@ class HolidayPolicy < ActiveRecord::Base
   belongs_to :account
   validates :name, presence: true
   validates :country, inclusion: { in: :countries, allow_nil: true }
+  validates :region, inclusion: { in: :regions, allow_nil: true}
+  validates :country, presence: :true, if: :region
 
-  before_validation :downcase, if: :country
+  before_validation :downcase, if: :local_data?
 
   private
 
+  def local_data?
+    country || region
+  end
+
   def downcase
-    self[:country] = self[:country].downcase
+    self[:country] = self[:country].try(:downcase)
+    self[:region] = self[:region].try(:downcase)
   end
 
   def countries
     countries = []
     ISO3166::Country.translations.select { |key, value| countries.push(key.downcase) }
     countries
+  end
+
+  def regions
+    regions = []
+    if country && countries.include?(country)
+      ISO3166::Country.new(country).states.select { |key, value| regions.push(key.downcase) }
+    end
+    regions
   end
 end
--- spec/models/holiday_policy_spec.rb
+++ spec/models/holiday_policy_spec.rb
@@ -6,37 +6,77 @@ RSpec.describe HolidayPolicy, type: :model do
   it { is_expected.to have_db_column(:region) }
   it { is_expected.to validate_presence_of(:name) }
 
-  context 'country inclusion' do
-    it 'should be valid when country code not send' do
-      params = { name: 'test' }
-      holiday_policy = HolidayPolicy.new(params)
-
-      expect(holiday_policy.valid?).to eq true
-      expect { holiday_policy.save }.to change { HolidayPolicy.count }.from(0).to(1)
-    end
-
-    it 'should be valid when valid country code send' do
-      params = { name: 'test', country: 'PL' }
-      holiday_policy = HolidayPolicy.new(params)
-
-      expect(holiday_policy.valid?).to eq true
-      expect { holiday_policy.save }.to change { HolidayPolicy.count }.from(0).to(1)
-    end
-
-    it 'should be valid when valid country code send' do
-      params = { name: 'test', country: 'pl' }
-      holiday_policy = HolidayPolicy.new(params)
-
-      expect(holiday_policy.valid?).to eq true
-      expect { holiday_policy.save }.to change { HolidayPolicy.count }.from(0).to(1)
-    end
-
-
-    it 'should not be valid when invalid country code send' do
-      params = { name: 'test', country: 'XYZ' }
+  context 'validations' do
+    context 'country presence' do
+      it 'should not be valid when region send but not country' do
+      params = { name: 'test', region: 'pl' }
       holiday_policy = HolidayPolicy.new(params)
 
       expect(holiday_policy.valid?).to eq false
+      end
+    end
+
+    context 'country inclusion' do
+      it 'should be valid when country code not send' do
+        params = { name: 'test' }
+        holiday_policy = HolidayPolicy.new(params)
+
+        expect(holiday_policy.valid?).to eq true
+        expect { holiday_policy.save }.to change { HolidayPolicy.count }.from(0).to(1)
+      end
+
+      it 'should be valid when valid country code send capitalized' do
+        params = { name: 'test', country: 'PL' }
+        holiday_policy = HolidayPolicy.new(params)
+
+        expect(holiday_policy.valid?).to eq true
+        expect { holiday_policy.save }.to change { HolidayPolicy.count }.from(0).to(1)
+      end
+
+      it 'should be valid when valid country code send not downcased' do
+        params = { name: 'test', country: 'pl' }
+        holiday_policy = HolidayPolicy.new(params)
+
+        expect(holiday_policy.valid?).to eq true
+        expect { holiday_policy.save }.to change { HolidayPolicy.count }.from(0).to(1)
+      end
+
+      it 'should not be valid when invalid country code send' do
+        params = { name: 'test', country: 'XYZ' }
+        holiday_policy = HolidayPolicy.new(params)
+
+        expect(holiday_policy.valid?).to eq false
+      end
+    end
+
+    context 'region inclusion' do
+      it 'should be valid when valid region code send capitalized' do
+        params = { name: 'test', country: 'pl', region: 'ds' }
+        holiday_policy = HolidayPolicy.new(params)
+
+        expect(holiday_policy.valid?).to eq true
+      end
+
+      it 'should be valid when valid region code send downcased' do
+        params = { name: 'test', country: 'pl', region: 'DS' }
+        holiday_policy = HolidayPolicy.new(params)
+
+        expect(holiday_policy.valid?).to eq true
+      end
+
+      it 'should not be valid when country code not send' do
+        params = { name: 'test', region: 'DS' }
+        holiday_policy = HolidayPolicy.new(params)
+
+        expect(holiday_policy.valid?).to eq false
+      end
+
+      it 'should not be valid when wrong country code send' do
+        params = { name: 'test', country: 'xyz', region: 'DS' }
+        holiday_policy = HolidayPolicy.new(params)
+
+        expect(holiday_policy.valid?).to eq false
+      end
     end
   end
 end
