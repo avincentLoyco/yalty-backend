2b7768700dfb6820eaa958ecbcc39cf444f77ba1
Magdalena Poprawa <magdalena.poprawa@monterail.com>
2015-09-28 10:45:46 +0200

    [YWA-49] Refactor HolidayPolicy

--- app/models/holiday_policy.rb
+++ app/models/holiday_policy.rb
@@ -2,33 +2,30 @@ class HolidayPolicy < ActiveRecord::Base
   belongs_to :account
   validates :name, presence: true
   validates :country, inclusion: { in: :countries, allow_nil: true }
-  validates :region, inclusion: { in: :regions, allow_nil: true}
+  validates :region, inclusion: { in: :regions, allow_nil: true}, if: :valid_country?
   validates :country, presence: :true, if: :region
-
-  before_validation :downcase, if: :local_data?
+  before_validation :downcase, if: :is_local?
 
   private
 
-  def local_data?
+  def is_local?
     country || region
   end
 
+  def valid_country?
+    country.present? && countries.include?(country)
+  end
+
   def downcase
-    self[:country] = self[:country].try(:downcase)
-    self[:region] = self[:region].try(:downcase)
+    self[:country] = country.try(:downcase)
+    self[:region] = region.try(:downcase)
   end
 
   def countries
-    countries = []
-    ISO3166::Country.translations.select { |key, value| countries.push(key.downcase) }
-    countries
+    ISO3166::Country.translations.keys.map &:downcase
   end
 
   def regions
-    regions = []
-    if country && countries.include?(country)
-      ISO3166::Country.new(country).states.select { |key, value| regions.push(key.downcase) }
-    end
-    regions
+    ISO3166::Country.new(country).states.keys.map &:downcase
   end
 end
