3788195d7cfa45e12460cb25d555cdc6465922aa
Jakub Cieslar <cieslar.jakub@gmail.com>
2015-09-29 13:13:16 +0200

    [YWA-53] Assign account to policy

--- app/resources/api/v1/holiday_policy_resource.rb
+++ app/resources/api/v1/holiday_policy_resource.rb
@@ -5,6 +5,10 @@ module API
       has_many :holidays, class_name: 'Holiday'
       has_many :employees, class_name: 'Employee'
       has_many :working_places, class_name: 'WorkingPlace'
+      has_one :assigned_account,
+        class_name: 'Settings',
+        foreign_key: 'holiday_policy_id',
+        foreign_key_on: :related
       attribute :name
 
       def self.records(options = {})
