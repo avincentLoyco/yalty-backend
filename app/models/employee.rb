91b3e9795d244b1c4cf77b512a68ed96f21b905f
Yann Lugrin <yann.lugrin@yalty.io>
2015-09-30 09:45:10 +0200

    Ensure Employee have an account

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,5 @@
 class Employee < ActiveRecord::Base
-  belongs_to :account, inverse_of: :employees
+  belongs_to :account, inverse_of: :employees, required: true
   belongs_to :working_place, inverse_of: :employees
   has_many :employee_attribute_versions, class_name: 'Employee::AttributeVersion', inverse_of: :employee
   has_many :employee_attributes, class_name: 'Employee::Attribute', inverse_of: :employee
