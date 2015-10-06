20a52190e354f60e229f33b688d9d1adda3d29e4
Magdalena Poprawa <magdalena.poprawa@monterail.com>
2015-10-06 10:40:08 +0200

    [YWA-72] Rewrite show and index in WorkingPlacescontroller

--- app/controllers/api/v1/working_places_controller.rb
+++ app/controllers/api/v1/working_places_controller.rb
@@ -1,6 +1,24 @@
 module API
   module V1
-    class WorkingPlacesController < JSONAPI::ResourceController
+    class WorkingPlacesController < ApplicationController
+
+      def show
+        render json: WorkingPlaceRepresenter.new(working_place).basic
+      end
+
+      def index
+        render json: WorkingPlacesRepresenter.new(working_places).basic
+      end
+
+      private
+
+      def working_place
+        @working_place ||= Account.current.working_places.find(params[:id])
+      end
+
+      def working_places
+        @working_places ||= Account.current.working_places
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # API
   namespace :api, path: 'api', constraints: { subdomain: /^api/ } do
     namespace :v1 do
+      resources :working_places, only: [:show, :index, :create, :update, :destroy]
+
       jsonapi_resources :employees do
         jsonapi_relationships
         jsonapi_related_resource :holiday_policy
@@ -9,9 +11,6 @@ Rails.application.routes.draw do
       jsonapi_resources :employee_attributes
       jsonapi_resources :employee_events
       jsonapi_resources :employee_attribute_definitions
-      jsonapi_resources :working_places do
-        jsonapi_relationships
-      end
       jsonapi_resource :settings, only: [:show, :update] do
         collection do
           put :'assign-holiday-policy'
