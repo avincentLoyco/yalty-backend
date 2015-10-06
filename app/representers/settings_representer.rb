2fad84809e8f731644c19a3c8c2506d0f1c68257
Jakub Cieslar <cieslar.jakub@gmail.com>
2015-10-06 10:33:43 +0200

    [YWA-71] Add settings representer

---
+++ app/representers/settings_representer.rb
@@ -0,0 +1,28 @@
+class SettingsRepresenter
+  attr_accessor :settings
+
+  def initialize(settings)
+    @settings = settings
+  end
+
+  def basic
+    {
+      type: 'settings',
+    }
+  end
+
+  def complete
+    {
+      subdomain: settings.subdomain,
+      company_name: settings.company_name,
+      timezone: settings.timezone,
+      default_locale: settings.default_locale,
+    }.merge(basic).merge(relationships)
+  end
+
+  def relationships
+    {
+      holiday_policy: HolidayPolicyRepresenter.new(settings.holiday_policy).basic,
+    }
+  end
+end
