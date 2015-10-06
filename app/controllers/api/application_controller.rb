aba8ce28fe324dd7c6aacc6f830b4630704b7a5a
Magdalena Poprawa <magdalena.poprawa@monterail.com>
2015-10-06 16:20:57 +0200

    [YWA-72] Add create, destroy and update to WorkingPlacesController

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,3 +1,18 @@
 class API::ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+
+  def with_verified_params(rules)
+    result = rules.verify(params)
+    if result.valid?
+      yield(result.attributes)
+    else
+      render json: ErrorsRepresenter.new(result.errors, 'settings').resource, status: 422
+    end
+  end
+
+  def gate_member_rule
+    Gate.rules do
+      required :id, :String
+    end
+  end
 end
--- app/controllers/api/v1/working_places_controller.rb
+++ app/controllers/api/v1/working_places_controller.rb
@@ -1,16 +1,84 @@
 module API
   module V1
     class WorkingPlacesController < ApplicationController
-
       def show
-        render json: WorkingPlaceRepresenter.new(working_place).basic
+        rules = gate_member_rule
+
+        with_verified_params(rules) do |attributes|
+          render json: WorkingPlaceRepresenter.new(working_place).basic
+        end
       end
 
       def index
         render json: WorkingPlacesRepresenter.new(working_places).basic
       end
 
+      def create
+        rules = Gate.rules do
+          required :name, :String
+          optional :employees, :Array
+          optional :type, :String
+        end
+
+        with_verified_params(rules) do |attributes|
+          if attributes[:employees]
+            attributes[:employees] = valid_employees(attributes[:employees])
+          end
+          working_place = Account.current.working_places.create(attributes)
+          if working_place.save
+            render json: WorkingPlaceRepresenter.new(working_place).basic
+          else
+            render json: ErrorsRepresenter.new(working_place.errors.messages, 'settings').resource,
+              status: 422
+          end
+        end
+      end
+
+      def update
+        rules = Gate.rules do
+          required :id, :String
+          optional :name, :String
+          optional :employees, :Array
+          optional :type, :String
+        end
+
+        with_verified_params(rules) do |attributes|
+          if attributes[:employees]
+            assign_employees(attributes[:employees])
+            attributes.delete(:employees)
+          end
+          if working_place.update(attributes)
+            render json: WorkingPlaceRepresenter.new(working_place).basic
+          else
+            render json: ErrorsRepresenter.new(working_place.errors.messages, 'settings').resource,
+              status: 422
+          end
+        end
+      end
+
+      def destroy
+        rules = gate_member_rule
+
+        with_verified_params(rules) do |attributes|
+          working_place.destroy!
+          head 204
+        end
+      end
+
       private
+
+      def assign_employees(employees)
+        working_place.employees.push(valid_employees(employees))
+      end
+
+      def valid_employees(employees)
+        employee_ids = employees.map { |employee| employee[:id] }
+        employees = []
+        employee_ids.each do |id|
+          employees.push(Account.current.employees.find(id))
+        end
+        employees
+      end
 
       def working_place
         @working_place ||= Account.current.working_places.find(params[:id])
