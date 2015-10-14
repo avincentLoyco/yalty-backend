69536ef29a559847d820957cabf82580d3ba01e1
Jakub Cieslar <cieslar.jakub@gmail.com>
2015-10-14 17:32:03 +0200

    [refactoring] Remove gem jsonapi-resources

--- app/resources/api/v1/setting_resource.rb
+++
@@ -1,9 +0,0 @@
-module API
-  module V1
-    class SettingResource < JSONAPI::Resource
-      model_name 'Account'
-      attributes :subdomain, :company_name, :timezone, :default_locale, :holiday_policy_id
-      key_type :integer
-    end
-  end
-end
