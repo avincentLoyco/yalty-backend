cadbd92c25019d1c8e52e2f584cc9449e224881f
MadMadlen <magdalenapoprawa@gmail.com>
2015-10-26 11:00:07 +0100

    [YWA-86] Add assosiation between PresencePolicy and Employees, WorkingPlace

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,6 +2,7 @@ class Employee < ActiveRecord::Base
   belongs_to :account, inverse_of: :employees, required: true
   belongs_to :working_place, inverse_of: :employees
   belongs_to :holiday_policy
+  belongs_to :presence_policy
   has_many :employee_attribute_versions,
     class_name: 'Employee::AttributeVersion',
     inverse_of: :employee
