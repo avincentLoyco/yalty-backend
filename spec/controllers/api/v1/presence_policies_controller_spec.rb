f5edb10294e29e19af2bc3953ee3beeb95666fca
MadMadlen <magdalenapoprawa@gmail.com>
2015-10-27 15:07:57 +0100

    [refactor] Introduce subject to PresencePoliciesController specs

--- spec/controllers/api/v1/presence_policies_controller_spec.rb
+++ spec/controllers/api/v1/presence_policies_controller_spec.rb
@@ -5,76 +5,61 @@ RSpec.describe API::V1::PresencePoliciesController, type: :controller do
 
   describe 'GET #show' do
     let(:presence_policy) { create(:presence_policy, account: account) }
+    subject { get :show, id: presence_policy.id }
 
     context 'valid data' do
-      it 'should respond with success' do
-        get :show, id: presence_policy.id
+      it { is_expected.to have_http_status(200) }
 
-        expect(response).to have_http_status(200)
-      end
+      context 'response body' do
+        before { subject }
 
-      it 'should return presence policy attributes' do
-        get :show, id: presence_policy.id
-
-        expect_json(id: presence_policy.id, type: 'presence_policy', name: presence_policy.name)
+        it { expect_json(id: presence_policy.id, name: presence_policy.name) }
       end
     end
 
     context 'invalid data' do
-      it 'should respond with 404 when invalid id given' do
-        get :show, id: '1'
+      context 'invalid id' do
+        subject { get :show, id: '1' }
 
-        expect(response).to have_http_status(404)
+        it { is_expected.to have_http_status(404) }
       end
 
-      it 'should respond with 404 when not account presence policy' do
-        user = create(:account_user)
-        Account.current = user.account
+      context 'not account presence policy' do
+        before(:each) do
+          user = create(:account_user)
+          Account.current = user.account
+        end
 
-        get :show, id: presence_policy.id
-        expect(response).to have_http_status(404)
+        it { is_expected.to have_http_status(404) }
       end
     end
   end
 
   describe 'GET #index' do
-    before(:each) do
-      create_list(:presence_policy, 3, account: account)
+    subject { get :index }
+    let!(:presence_policies) { create_list(:presence_policy, 3, account: account) }
+
+    context 'account presence policy' do
+      before { subject }
+
+      it { expect_json_sizes(3) }
+      it { is_expected.to have_http_status(200) }
+      it { expect_json_types('*', name: :string, id: :string, type: :string) }
     end
 
-    it 'should respond with success' do
-      get :index
+    context 'not account presence policy' do
+      before(:each) do
+        user = create(:account_user)
+        Account.current = user.account
+      end
 
-      expect(response).to have_http_status(200)
-    end
+      context 'response' do
+        before { subject }
 
-    it 'should respond with account presence polciies list' do
-      get :index
-
-      expect_json_sizes(3)
-    end
-
-    it 'should return presence policy attributes' do
-      get :index
-
-      expect_json_types('*', name: :string, id: :string, type: :string)
-    end
-
-    it 'should return empty array when account does not have any presence policy' do
-      Account.current.presence_policies.destroy_all
-
-      get :index
-      expect_json_sizes(0)
-      expect(response.body).to eq [].to_json
-    end
-
-    it 'should not be visible in context of other account' do
-      user = create(:account_user)
-      Account.current = user.account
-
-      get :index
-      expect(response).to have_http_status(200)
-      expect_json_sizes(0)
+        it { expect_json_sizes(0) }
+        it { expect(response.body).to eq [].to_json }
+        it { expect(response).to have_http_status(200) }
+      end
     end
   end
 
@@ -317,67 +302,35 @@ RSpec.describe API::V1::PresencePoliciesController, type: :controller do
 
   describe 'DELETE #destroy' do
     let!(:presence_policy) { create(:presence_policy, account: account) }
+    subject { delete :destroy, id: presence_policy.id }
 
     context 'valid data' do
-      it 'should destroy resource' do
-        expect { delete :destroy, id: presence_policy.id }
-          .to change { PresencePolicy.count }.by(-1)
-      end
-
-      it 'should respond with success' do
-        delete :destroy, id: presence_policy.id
-
-        expect(response).to have_http_status(204)
-      end
+      it { expect { subject }.to change { PresencePolicy.count }.by(-1) }
+      it { is_expected.to have_http_status(204) }
     end
 
     context 'invalid data' do
       context 'presence policy has working places assigned' do
         let(:working_place) { create(:working_place, account: account) }
+        before { presence_policy.working_places.push(working_place) }
 
-        it 'should not destroy presence policy' do
-          presence_policy.working_places.push(working_place)
-
-          expect { delete :destroy, id: presence_policy.id }
-            .to_not change { PresencePolicy.count }
-        end
-
-        it 'should respond with status 423' do
-          presence_policy.working_places.push(working_place)
-          delete :destroy, id: presence_policy.id
-
-          expect(response).to have_http_status(423)
-        end
+        it { expect { subject }.to_not change { PresencePolicy.count } }
+        it { is_expected.to have_http_status(423) }
       end
 
       context 'presence policy has employees assigned' do
         let(:employee) { create(:employee, account: account) }
+        before { presence_policy.employees.push(employee) }
 
-        it 'should not destroy presence policy' do
-          presence_policy.employees.push(employee)
-
-          expect { delete :destroy, id: presence_policy.id }
-            .to_not change { PresencePolicy.count }
-        end
-
-        it 'should respond with status 423' do
-          presence_policy.employees.push(employee)
-          delete :destroy, id: presence_policy.id
-
-          expect(response).to have_http_status(423)
-        end
+        it { expect { subject }.to_not change { PresencePolicy.count } }
+        it { is_expected.to have_http_status(423) }
       end
 
       context 'invalid id' do
-        it 'should not destroy presence policy' do
-          expect { delete :destroy, id: '1' }.to_not change { PresencePolicy.count }
-        end
+        subject { delete :destroy, id: '1' }
 
-        it 'should respond with 404' do
-          delete :destroy, id: '1'
-
-          expect(response).to have_http_status(404)
-        end
+        it { expect { subject }.to_not change { PresencePolicy.count } }
+        it { is_expected.to have_http_status(404) }
       end
 
       context 'presence policy belongs to other account' do
@@ -386,15 +339,8 @@ RSpec.describe API::V1::PresencePoliciesController, type: :controller do
           Account.current = user.account
         end
 
-        it 'should not destroy presence policy' do
-          expect { delete :destroy, id: presence_policy.id }.to_not change { PresencePolicy.count }
-        end
-
-        it 'should respond with 404' do
-          delete :destroy, id: presence_policy.id
-
-          expect(response).to have_http_status(404)
-        end
+        it { expect { subject }.to_not change { PresencePolicy.count } }
+        it { is_expected.to have_http_status(404) }
       end
     end
   end
