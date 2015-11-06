ea2b45ef02392a1baf9eb14d2ab69d611d9c0b4f
Jakub Cieslar <cieslar.jakub@gmail.com>
2015-11-06 13:25:08 +0100

    [YWA-60] Update attribute definiton rules and representer with multiple

--- app/rules/employee_attribute_definition_rules.rb
+++ app/rules/employee_attribute_definition_rules.rb
@@ -1,22 +1,13 @@
 module EmployeeAttributeDefinitionRules
   include BaseRules
 
-  def patch_rules
-    Gate.rules do
-      required :id
-      optional :name
-      optional :label
-      optional :attribute_type
-      optional :system
-    end
-  end
-
   def post_rules
     Gate.rules do
       required :name
       optional :label
       required :attribute_type
       required :system
+      optional :multiple
     end
   end
 
