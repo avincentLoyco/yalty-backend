71879df5d17a56982dbc96b2ede2969e089580b2
MadMadlen <magdalena.poprawa@monterail.com>
2015-11-18 14:44:42 +0100

    [YWA-104] Add tests for Account::RegistrationKey scope

--- spec/models/account/registration_key_spec.rb
+++ spec/models/account/registration_key_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe Account::RegistrationKey, type: :model do
 
     it { expect { registration_key.valid? }.to change { registration_key.token }.from(nil) }
   end
+
+  context 'scope' do
+    let!(:key_without_account) { create_list(:registration_key, 2) }
+    let!(:key_with_account) { create(:registration_key, :with_account) }
+    let!(:key_with_account_zero) { create(:registration_key, account_id: "0") }
+
+    it { expect(Account::RegistrationKey.count).to eql(4) }
+    it { expect(Account::RegistrationKey.unused.count).to eql(2) }
+  end
 end
