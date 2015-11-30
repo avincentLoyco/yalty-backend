5e9e3cf16249f9530e90c6d6c2ca226225d9588f
MadMadlen <magdalena.poprawa@monterail.com>
2015-11-30 10:35:40 +0100

    [specs] Add stub for update_default_attribute_definitions
    Specs are much slower, because of after_create in account model.
    Thanks to this stub there are almost 3 minutes faster.
    Stub is not called for EmployeeAttributesDefinitionController

--- spec/support/shared_context_headers.rb
+++ spec/support/shared_context_headers.rb
@@ -2,6 +2,12 @@ RSpec.shared_context 'shared_context_headers', :a => :b do
   let(:user) { create(:account_user) }
   let(:account) { user.account }
 
+  unless described_class == API::V1::EmployeeAttributeDefinitionsController
+    before do
+      allow_any_instance_of(Account).to receive(:update_default_attribute_definitions!) { true }
+    end
+  end
+
   before(:each) do
     Account::User.current = user
     Account.current = account
