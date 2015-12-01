524046e13a841cb7dd6998e14511e0b967e3346d
MadMadlen <magdalena.poprawa@monterail.com>
2015-12-01 13:08:25 +0100

    [YWA-143][WIP] Add gate rules for nested attributes

--- app/rules/employee_attribute_version_rules.rb
+++ app/rules/employee_attribute_version_rules.rb
@@ -5,9 +5,14 @@ class EmployeeAttributeVersionRules
   end
 
   def post_rules
+    values = nested_values
     Gate.rules do
       required :attribute_name
-      required :value, allow_nil: true
+      required :value, allow_nil: true do
+        values.flatten.each do |value|
+          optional value
+        end
+      end
       optional :order
     end
   end
@@ -18,6 +23,14 @@ class EmployeeAttributeVersionRules
       required :value, allow_nil: true
       required :attribute_name
       optional :order
+    end
+  end
+
+  def nested_values
+    %w(Address Child Person).map do |type|
+      "Attribute::#{type}".constantize.new.attributes.tap do |attr|
+        attr.delete(:attribute_type)
+      end.keys
     end
   end
 end
