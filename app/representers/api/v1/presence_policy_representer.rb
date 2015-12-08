325866690f53f7781cdd7bc0002da1fbac3dbb2c
MadMadlen <magdalena.poprawa@monterail.com>
2015-12-08 16:49:15 +0100

    [YWA-153] Add working places and employees to presence policy json payload

--- app/representers/api/v1/presence_policy_representer.rb
+++ app/representers/api/v1/presence_policy_representer.rb
@@ -8,10 +8,27 @@ module Api::V1
     end
 
     def with_relationships
-      response = resource.presence_days.map do |attribute|
+      complete.merge(presence_days: presence_days_json)
+        .merge(employees: employees_json)
+        .merge(working_places: working_places_json)
+    end
+
+    def presence_days_json
+      resource.presence_days.map do |attribute|
         PresenceDayRepresenter.new(attribute).complete
       end
-      complete.merge(presence_days: response)
+    end
+
+    def employees_json
+      resource.employees.map do |attribute|
+        EmployeeRepresenter.new(attribute).basic
+      end
+    end
+
+    def working_places_json
+      resource.working_places.map do |attribute|
+        WorkingPlaceRepresenter.new(attribute).basic
+      end
     end
   end
 end
