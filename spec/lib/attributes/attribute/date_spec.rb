d341e4806b358d5612e539a998e4a2cb379e3460
MadMadlen <magdalena.poprawa@monterail.com>
2015-12-08 16:12:35 +0100

    [YWA-138] Add specs for attribute types presence

--- spec/lib/attributes/attribute/date_spec.rb
+++ spec/lib/attributes/attribute/date_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Attribute::Date do
   it { is_expected.to be_respond_to(:date) }
+  it { is_expected.to validate_presence_of(:date) }
 end
--- spec/lib/attributes/attribute/number_spec.rb
+++ spec/lib/attributes/attribute/number_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Attribute::Number do
   it { is_expected.to be_respond_to(:number) }
+  it { is_expected.to validate_presence_of(:number) }
 end
