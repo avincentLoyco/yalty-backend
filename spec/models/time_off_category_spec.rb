67294ac28ab58306cdffbdcc45dca8c01a4592f3
MadMadlen <magdalena.poprawa@monterail.com>
2015-12-10 11:17:04 +0100

    [YWA-149] Add more specs for TimeOffCategory

--- spec/models/time_off_category_spec.rb
+++ spec/models/time_off_category_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe TimeOffCategory, type: :model do
+  it { is_expected.to have_db_column(:id).of_type(:uuid) }
+  it { is_expected.to have_db_column(:account_id).of_type(:uuid).with_options(null: false) }
+  it { is_expected.to have_db_column(:name).of_type(:string).with_options(null: false) }
+  it { is_expected.to have_db_column(:system).of_type(:boolean).with_options(null: false) }
 
-  it { is_expected.to have_db_column(:account_id).of_type(:uuid) }
-  it { is_expected.to have_db_column(:name).of_type(:string) }
-  it { is_expected.to have_db_column(:system).of_type(:boolean) }
+  it { is_expected.to have_db_index(:account_id) }
 
   it { is_expected.to validate_presence_of(:account) }
   it { is_expected.to validate_presence_of(:name) }
