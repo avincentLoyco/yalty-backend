bd1f55f045450ad85437b295f3178975d17f1831
Yann Lugrin <yann.lugrin@yalty.io>
2015-12-16 10:29:41 +0100

    Only report error to newrelic if exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,8 +40,8 @@ class ApplicationController < ActionController::Base
       ::Api::V1::ErrorsRepresenter.new(resource, message).complete, status: 404
   end
 
-  def render_500_error(exception)
-    NewRelic::Agent.notice_error(exception)
+  def render_500_error(exception = nil)
+    NewRelic::Agent.notice_error(exception) if exception
     resource = { resource: 'internal_server_error' }
     render json:
       ::Api::V1::ErrorsRepresenter.new(nil, resource).complete, status: 500
