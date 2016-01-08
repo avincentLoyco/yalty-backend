1b404581ec8abfa8f14e95e1fe7c8a1732bf5a14
MadMadlen <magdalena.poprawa@monterail.com>
2016-01-08 16:38:17 +0100

    [YWA-157] Create TimeOffPolicy table

---
+++ db/migrate/20160108110841_create_time_off_policy.rb
@@ -0,0 +1,17 @@
+class CreateTimeOffPolicy < ActiveRecord::Migration
+  def up
+    create_table :time_off_policies, id: :uuid do |t|
+      t.date :start_time, null: false
+      t.date :end_time, null: false
+      t.integer :amount
+      t.string :type, null: false
+      t.uuid :time_off_category_id, null: false
+    end
+    add_foreign_key :time_off_policies, :time_off_categories, on_delete: :cascade, column: :time_off_category_id
+    add_index :time_off_policies, :time_off_category_id
+  end
+
+  def down
+    drop_table :time_off_policies
+  end
+end
