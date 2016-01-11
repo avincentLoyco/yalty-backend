70b5346402adfb95c784d4f1f00de1ce17e007bc
Jakub Cieslar <cieslar.jakub@gmail.com>
2016-01-11 12:00:34 +0100

    [YWA-204] Add update spec for user API

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe API::V1::UsersController, type: :controller do
   let(:employee) { create(:employee, account: account) }
   let(:employee_id) { employee.id }
 
-
   describe 'POST #create' do
     let(:email) { 'test@example.com' }
     let(:password) { '12345678' }
@@ -87,7 +86,70 @@ RSpec.describe API::V1::UsersController, type: :controller do
     end
   end
 
+  describe 'GET #index' do
+    let!(:users) { create_list(:account_user, 3, account: Account.current) }
+
+    subject { get :index }
+
+    context 'user list' do
+      before { subject }
+
+      it { expect_json_sizes(users.count + 1) }
+      it { is_expected.to have_http_status(200) }
+    end
+  end
+
+  describe 'GET #show' do
+    let!(:users) { create_list(:account_user, 3, account: Account.current) }
+    let(:params) {{ id: users.first.id}}
+
+    subject { get :show, params }
+
+    context 'show user' do
+      before { subject }
+
+      it { is_expected.to have_http_status(200) }
+    end
+  end
+
   describe 'PUT #update' do
+    let!(:users) { create_list(:account_user, 3, account: Account.current) }
+    let(:email) { 'test123@example.com' }
+    let(:account_manager) { true }
+    let(:params) do
+      {
+        id: users.first.id,
+        email: email,
+        account_manager: account_manager,
+        employee: { id: employee_id }
+      }
+    end
+
+    subject { put :update, params }
+
+    context 'with valid data' do
+      it { expect { subject }.to_not change { Account::User.count } }
+      it { is_expected.to have_http_status(204) }
+      it { expect { subject }.to change { users.first.reload.email } }
+
+      context 'assign employee' do
+        it { expect { subject }.to change { employee.reload.account_user_id } }
+      end
+
+      context 'without employee id' do
+        before do
+          params.delete(:employee)
+        end
+
+        it { is_expected.to have_http_status(204) }
+        it { expect { subject }.to_not change { Account::User.count } }
+
+        context 'with blank id' do
+          it { expect { subject }.to_not change { employee.reload.account_user_id } }
+        end
+      end
+    end
+
 
   end
 end
