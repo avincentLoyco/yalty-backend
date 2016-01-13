974c0ee0ceb0b36c65bbd3dbcba00e883adf464e
Daniel <daniel.montenegro@monterail.com>
2016-01-13 16:42:41 +0100

    [Fix] Removal of unnecessary validation from Balance

--- app/models/employee/balance.rb
+++ app/models/employee/balance.rb
@@ -4,6 +4,5 @@ class Employee::Balance < ActiveRecord::Base
   belongs_to :time_off
   belongs_to :time_off_policy
 
-  validates :employee, :time_off_category, :balance, presence: true
-  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }, if: :balance?
+  validates :employee, :time_off_category, :balance, :amount, presence: true
 end
