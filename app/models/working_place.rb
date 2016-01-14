018d7e3cd2398dcc21351339af307268e0ee34fb
Daniel <daniel.montenegro@monterail.com>
2016-01-14 18:48:47 +0100

    [FIX] Relocation of wrongly placed has_many attribute

--- app/models/working_place.rb
+++ app/models/working_place.rb
@@ -3,6 +3,7 @@ class WorkingPlace < ActiveRecord::Base
   belongs_to :holiday_policy
   belongs_to :presence_policy
   has_many :employees, inverse_of: :working_place
+  has_many :working_place_time_off_policies
 
   validates :name, :account_id, presence: true
 end
