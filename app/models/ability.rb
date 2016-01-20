7f1365e2355916347ccd149740054b73bf23637d
Jakub Cieslar <cieslar.jakub@gmail.com>
2016-01-20 14:28:31 +0100

    [hotfix] Add proper user namespace in ability model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,7 +8,7 @@ class Ability
       can :read, WorkingPlace
       can [:show, :index], Employee
       can :update, Employee, account_user_id: user.id
-      can :update, User, id: user.id
+      can :update, Account::User, id: user.id
     end
   end
 end
