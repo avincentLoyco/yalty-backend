be7784e09f63f6e21ea457f312d241c661217e1b
Daniel <daniel.montenegro@monterail.com>
2016-01-22 12:42:36 +0100

    [YWA-160] Create TimeOffPolicy respresenter

--- app/representers/api/v1/time_off_policy_representer.rb
+++ app/representers/api/v1/time_off_policy_representer.rb
@@ -2,7 +2,13 @@ module Api::V1
   class TimeOffPolicyRepresenter < BaseRepresenter
     def complete
       {
-
+        start_day: resource.start_day,
+        end_day: resource.end_day,
+        start_month: resource.start_month,
+        end_month: resource.end_month,
+        amount: resource.amount,
+        policy_type: resource.policy_type,
+        years_to_effect: resource.years_to_effect,
       }
         .merge(basic)
         .merge(relationships)
@@ -10,8 +16,26 @@ module Api::V1
 
     def relationships
       {
-        
+        time_off_category: time_off_category_json,
+        employees: employees_json,
+        working_places: working_places_json,
       }
+    end
+
+    def time_off_category_json
+      TimeOffCategoryRepresenter.new(resource.time_off_category).basic
+    end
+
+    def employees_json
+      resource.employees.map do |employee|
+        EmployeeRepresenter.new(employee).basic
+      end
+    end
+
+    def working_places_json
+      resource.working_places.map do |working_place|
+        WorkingPlaceRepresenter.new(working_place).basic
+      end
     end
   end
 end
