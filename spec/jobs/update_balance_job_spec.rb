82d1ac2475717279debbb6927b97af649801a706
MadMadlen <magdalena.poprawa@monterail.com>
2016-01-25 17:54:11 +0100

    [YWA-162] Add specs for Create and Update Balance Jobs

---
+++ spec/jobs/update_balance_job_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe UpdateBalanceJob do
+  subject { UpdateBalanceJob.perform_now(ids, 100) }
+
+  let(:first_balance) { create(:employee_balance, beeing_processed: true) }
+  let(:second_balance) { first_balance.dup }
+  let(:third_balance) { first_balance.dup}
+  let(:ids) { Employee::Balance.pluck(:id) }
+
+  before do
+    second_balance.update!(created_at: Time.now + 1.week, updated_at: Time.now + 1.week)
+    third_balance.update!(created_at: Time.now + 1.month, updated_at: Time.now + 1.month)
+  end
+
+  describe '#perform' do
+    shared_examples 'Balance and Beeing Processed Status Change' do
+      it { expect { subject }.to change { second_balance.reload.balance } }
+      it { expect { subject }.to change { third_balance.reload.balance } }
+
+      it { expect { subject }.to change { second_balance.reload.beeing_processed }.to false }
+      it { expect { subject }.to change { third_balance.reload.beeing_processed }.to false }
+
+      it { expect { subject }.to_not change { second_balance.reload.amount } }
+      it { expect { subject }.to_not change { third_balance.reload.amount } }
+    end
+
+    context 'with amount params' do
+      it { expect { subject }.to change { first_balance.reload.balance } }
+      it { expect { subject }.to change { first_balance.reload.amount }.to(100) }
+      it { expect { subject }.to change { first_balance.reload.beeing_processed }.to false }
+
+      it_behaves_like 'Balance and Beeing Processed Status Change'
+    end
+
+    context 'without amount param' do
+      subject { UpdateBalanceJob.perform_now(ids) }
+      before { first_balance.destroy! }
+
+      it_behaves_like 'Balance and Beeing Processed Status Change'
+    end
+
+    context 'with time off id' do
+      subject { UpdateBalanceJob.perform_now(ids, 100, time_off.id) }
+      let(:time_off) { create(:time_off, employee: first_balance.employee, beeing_processed: true) }
+
+      it { expect { subject }.to change { time_off.reload.beeing_processed }.to false }
+    end
+  end
+end
