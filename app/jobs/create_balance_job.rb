423be51c7a08be94e0a8277e7e7302b270698087
MadMadlen <magdalena.poprawa@monterail.com>
2016-02-10 15:54:29 +0100

    [YWA-162] Remove CreateBalanceJob

--- app/jobs/create_balance_job.rb
+++
@@ -1,7 +0,0 @@
-class CreateBalanceJob < ActiveJob::Base
-  @queue = :balance
-
-  def perform(category_id, employee_id, account_id, amount, time_off_id = nil)
-    CreateEmployeeBalance.new(category_id, employee_id, account_id, amount, time_off_id).call
-  end
-end
