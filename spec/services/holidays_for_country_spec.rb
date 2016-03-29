e151348767bf120837327564a8e306f986d675bc
Daniel M <daniel.montenegro@monterail.com>
2016-03-29 15:58:39 +0200

    [Fix] Add of filter to surpress gem bug
    Avoid mistaken regions when asking for a country's holidays. Also a test was added to adress this case

--- spec/services/holidays_for_country_spec.rb
+++ spec/services/holidays_for_country_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe HolidaysForCountry, type: :service do
-  subject{ described_class.new('ch').call }
+  subject{ described_class.new('ch', true).call }
 
   describe '#call' do
+    before { described_class.new('at', false).call  }
     it { expect(subject.class).to be Array }
     it 'returns the holidays of a country and the regions with the holidays for each region' do |variable|
         holidays , regions_with_holidays = subject
@@ -11,6 +12,9 @@ RSpec.describe HolidaysForCountry, type: :service do
         expect(holidays.first).to include :code, :date
         expect(regions_with_holidays.class).to be Array
         expect(regions_with_holidays.first).to include :code, :holidays
+        regions_with_holidays.each do |region_hash|
+          expect(region_hash[:code]).to_not equal :at
+        end
     end
   end
 
