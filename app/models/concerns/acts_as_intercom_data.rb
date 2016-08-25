c14a7b9d8adbfadcd1b3eb8b4b6b97a495256067
Yann Lugrin <yann.lugrin@yalty.io>
2016-08-25 15:06:09 +0200

    Refactoring intercom delayed job check

--- app/models/concerns/acts_as_intercom_data.rb
+++ app/models/concerns/acts_as_intercom_data.rb
@@ -37,28 +37,11 @@ module ActsAsIntercomData
   private
 
   def job_not_on_queue
-    !send(self.class.name.downcase.split('::').last + '_delayed_jobs').any? { |job| job.match(id) }
-  end
-
-  def account_delayed_jobs
-    @account_delayed_jobs ||= delayed_jobs.map do |job|
-      job if job =~ /Account/ && !(job =~ /User/)
-    end.compact
-  end
-
-  def user_delayed_jobs
-    @user_delayed_jobs ||= delayed_jobs.map { |job| job if job =~ /User/ }.compact
-  end
-
-  def intercom_jobs
-    return unless delayed_jobs.present?
-
-    @intercom_jobs ||= delayed_jobs.map { |job| job if job =~ /intercom/ }
+    delayed_jobs.none? { |job| job.match(id) }
   end
 
   def delayed_jobs
-    @delayed_jobs ||= Resque.redis.keys('delayed:[0-9]*')
-      .map { |key| Resque.redis.lrange(key, 0, -1) }
-      .flatten
+    @delayed_jobs ||= Resque.redis.keys('timestamps:*')
+      .select_if {|key| key =~ /"queue":"intercom"/ && key =~ /"#{self.class.name}"/ }
   end
 end
