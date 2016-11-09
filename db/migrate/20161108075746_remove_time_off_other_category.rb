cb03b6b4d2382caad3824a773de52b2539227bd7
Mateusz Karbowiak <mateusz.karbowiak@monterail.com>
2016-11-09 15:07:40 +0100

    [FIX] Fix DELETE SQL query

--- db/migrate/20161108075746_remove_time_off_other_category.rb
+++ db/migrate/20161108075746_remove_time_off_other_category.rb
@@ -1,9 +1,14 @@
 class RemoveTimeOffOtherCategory < ActiveRecord::Migration
   def up
     execute("""
-      DELETE FROM time_off_categories toc
-      USING time_off_policies top
-      WHERE toc.id != top.time_off_category_id AND toc.name = 'other';
+      DELETE FROM time_off_categories WHERE time_off_categories.id IN (
+        SELECT time_off_categories.id FROM time_off_categories
+        LEFT JOIN time_off_policies
+        ON time_off_policies.time_off_category_id = time_off_categories.id
+        WHERE time_off_categories.name = 'other'
+        GROUP BY time_off_categories.id
+        HAVING COUNT(time_off_policies.id) = 0
+      );
 
       UPDATE time_off_categories SET system = false WHERE name = 'other';
     """)
