d1a5e2e4b62e4dc7edf97697413847ac5d4dcc6f
Mateusz Karbowiak <mateusz.karbowiak@monterail.com>
2016-11-15 10:11:55 +0100

    [YWA-831, YWA-831] Fixes after review

--- app/services/employee_balance/recreate_balances/after_etop_update.rb
+++ app/services/employee_balance/recreate_balances/after_etop_update.rb
@@ -1,6 +1,6 @@
 module RecreateBalances
   class AfterEtopUpdate
-    attr_reader :params
+    attr_reader :recreate_balances_helper, :remove_balances_service
 
     def initialize(
       new_effective_at:,
@@ -9,17 +9,25 @@ module RecreateBalances
       employee_id:,
       manual_amount: 0
     )
-      @params = {
+      @recreate_balances_helper = RecreateBalancesHelper.new(
         new_effective_at: new_effective_at,
         old_effective_at: old_effective_at,
         time_off_category_id: time_off_category_id,
         employee_id: employee_id,
         manual_amount: manual_amount
-      }
+      )
+      @remove_balances_service = RemoveBalances.new(
+        recreate_balances_helper.time_off_category,
+        recreate_balances_helper.employee,
+        recreate_balances_helper.starting_date,
+        recreate_balances_helper.ending_date
+      )
     end
 
     def call
-      RecreateBalancesHelper.new(**params).after_etop_update
+      remove_balances_service.call
+      recreate_balances_helper.remove_balance_at_old_effective_at!
+      recreate_balances_helper.recreate_and_recalculate_balances!
     end
   end
 end
