5ab70eb70d519c95b60eeafa8d3af66ac8bb4569
Yann Lugrin <yann.lugrin@yalty.io>
2017-02-28 15:11:59 +0100

    Do not set GOOGLE_API_KEY if not present

--- config/initializers/geolocation.rb
+++ config/initializers/geolocation.rb
@@ -1,4 +1,7 @@
-Geokit::Geocoders::GoogleGeocoder.api_key = ENV['GOOGLE_API_KEY']
-Timezone::Lookup.config(:google) do |c|
-  c.api_key = ENV['GOOGLE_API_KEY']
+if ENV['GOOGLE_API_KEY'].present?
+  Geokit::Geocoders::GoogleGeocoder.api_key = ENV['GOOGLE_API_KEY']
+
+  Timezone::Lookup.config(:google) do |c|
+    c.api_key = ENV['GOOGLE_API_KEY']
+  end
 end
