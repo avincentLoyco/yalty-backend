364e84edfd2742edbb451ed07177a2f5df5ba701
Michał Nowicki <michal.nowicki@monterail.com>
2017-04-03 14:50:45 +0200

    [YWA-1253] Add additional fields to Invoice
    Add receipt_number, starting_balance, subtotal, tax, tax_percent and
    total to Invoice
    Add 'billing' to sidekiq config.

--- spec/support/stripe_mocks.rb
+++ spec/support/stripe_mocks.rb
@@ -1,6 +1,7 @@
 StripeSubscription = Struct.new(:id, :current_period_end, :items, :status, :customer, :object)
 StripeCustomer = Struct.new(:id, :description, :default_source, :sources, :save)
-StripeInvoice = Struct.new(:id, :amount_due, :date, :lines, :upcoming, :data)
+StripeInvoice = Struct.new(:id, :amount_due, :date, :lines, :upcoming, :data, :receipt_number, :tax,
+  :tax_percent, :starting_balance, :subtotal, :total)
 StripePlan = Struct.new(:id, :amount, :currency, :interval, :name, :active)
 StripeCard = Struct.new(:id, :last4, :brand, :exp_month, :exp_year, :default, :name)
 StripeEvent = Struct.new(:id, :type)
