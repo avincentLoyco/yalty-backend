493d031178ab5ed805657f494c5867dc6f0bb8dd
Yann Lugrin <yann.lugrin@yalty.io>
2017-04-05 15:09:24 +0200

    Fix migrations for production

--- db/migrate/20170123075900_add_reset_flag_to_policies_and_working_places.rb
+++ db/migrate/20170123075900_add_reset_flag_to_policies_and_working_places.rb
@@ -1,7 +1,19 @@
 class AddResetFlagToPoliciesAndWorkingPlaces < ActiveRecord::Migration
   def change
-    add_column :working_places,    :reset, :boolean, default: false
-    add_column :presence_policies, :reset, :boolean, default: false
-    add_column :time_off_policies, :reset, :boolean, default: false
+    add_column :working_places,    :reset, :boolean
+    add_column :presence_policies, :reset, :boolean
+    add_column :time_off_policies, :reset, :boolean
+    execute <<-SQL
+      UPDATE working_places SET reset = false
+    SQL
+    change_column_null :working_places, :reset, true, false
+    execute <<-SQL
+      UPDATE presence_policies SET reset = false
+    SQL
+    change_column_null :presence_policies, :reset, true, false
+    execute <<-SQL
+      UPDATE time_off_policies SET reset = false
+    SQL
+    change_column_null :time_off_policies, :reset, true, false
   end
 end
--- db/migrate/20170301085412_add_invoice_address_and_emails_to_account.rb
+++ db/migrate/20170301085412_add_invoice_address_and_emails_to_account.rb
@@ -1,6 +1,10 @@
 class AddInvoiceAddressAndEmailsToAccount < ActiveRecord::Migration
   def change
     add_column :accounts, :invoice_company_info, :hstore
-    add_column :accounts, :invoice_emails, :text, array: true, default: []
+    add_column :accounts, :invoice_emails, :text, array: true
+    execute <<-SQL
+      UPDATE accounts SET invoice_emails = '{}'::text[]
+    SQL
+    change_column_null :accounts, :invoice_emails, true, []
   end
 end
