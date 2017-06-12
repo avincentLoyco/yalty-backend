e132ddef57676014e8e2a9c8c6df9cc566abbe4e
Yann Lugrin <yann.lugrin@yalty.io>
2017-06-12 15:44:16 +0200

    [YWA-1395] Use SFTP instead of SCP

--- app/jobs/export/send_employees_journal.rb
+++ app/jobs/export/send_employees_journal.rb
@@ -6,13 +6,15 @@ module Export
       Dir.mktmpdir(account.subdomain) do |tmp_dir_path|
         csv_file_path = ::Export::GenerateEmployeesJournal.new(account, tmp_dir_path).call
 
-        Net::SCP.upload!(
+        Net::SFTP.start(
           ENV['LOYCO_SSH_HOST'],
           ENV['LOYCO_SSH_USER'],
-          csv_file_path.to_s,
-          csv_file_path.split.last.to_s,
-          ssh: { keys: [ENV['LOYCO_SSH_KEY_PATH']] }
-        )
+          keys: [ENV['LOYCO_SSH_KEY_PATH']]
+        ) do |sftp|
+          sftp.upload!(
+            csv_file_path.to_s, File.join(ENV['LOYCO_SSH_PATH'], csv_file_path.basename.to_s)
+          )
+        end
       end
     end
   end
--- spec/jobs/export/send_employees_journal_spec.rb
+++ spec/jobs/export/send_employees_journal_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe Export::SendEmployeesJournal, type: :job do
   let(:ssh_path) { 'fakepath' }
   let(:filename) { Pathname.new('employees_journal.csv') }
   let(:path)     { Rails.root.join(filename) }
+  let(:sftp) do
+    sftp = double('sftp')
+    allow(sftp).to receive(:upload!)
+    sftp
+  end
 
   subject(:generate_and_send_journal) { described_class.new.perform(account) }
 
@@ -14,10 +19,12 @@ RSpec.describe Export::SendEmployeesJournal, type: :job do
     ENV['LOYCO_SSH_HOST'] = ssh_host
     ENV['LOYCO_SSH_USER'] = ssh_user
     ENV['LOYCO_SSH_KEY_PATH'] = ssh_path
+    ENV['LOYCO_SSH_PATH'] = '/'
     allow(::Export::GenerateEmployeesJournal).to receive_message_chain(:new, :call).and_return(path)
-    allow(Net::SCP)
-      .to receive(:upload!)
-      .with(ssh_host, ssh_user, path.to_s, filename.to_s, ssh: { keys: [ssh_path] })
+    allow(Net::SFTP)
+      .to receive(:start)
+      .with(ssh_host, ssh_user, keys: [ssh_path])
+      .and_yield(sftp)
   end
 
   it 'invokes spreadsheet creator' do
@@ -26,9 +33,7 @@ RSpec.describe Export::SendEmployeesJournal, type: :job do
   end
 
   it 'uploads journal to sftp server' do
-    expect(Net::SCP)
-      .to receive(:upload!)
-      .with(ssh_host, ssh_user, path.to_s, filename.to_s, ssh: { keys: [ssh_path] })
+    expect(sftp).to receive(:upload!).with(path.to_s, "/#{filename.to_s}")
     generate_and_send_journal
   end
 end
