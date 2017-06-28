8dc2c439d3fff11d35fb83b468d92cece996ef26
MadMadlen <magdalena.poprawa@monterail.com>
2017-06-28 13:32:52 +0200

    [YWA-1525] Fix restrictions for automated exports

--- app/jobs/export/schedule_employees_journal_export.rb
+++ app/jobs/export/schedule_employees_journal_export.rb
@@ -5,6 +5,7 @@ module Export
     def perform
       return unless ::Export::SendEmployeesJournal.enable?
       Account.with_yalty_access.find_each do |account|
+        next unless account.available_modules.include?('automatedexport')
         ::Export::SendEmployeesJournal.perform_later(account)
       end
     end
--- spec/jobs/export/schedule_employees_journal_export_spec.rb
+++ spec/jobs/export/schedule_employees_journal_export_spec.rb
@@ -17,14 +17,28 @@ RSpec.describe Export::ScheduleEmployeesJournalExport, type: :job do
   it { expect(account_with_yalty_access.yalty_access).to be(true) }
   it { expect(account_without_yalty_access.yalty_access).to be(false) }
 
-  it 'schedules export only once' do
-    expect(export_job).to receive(:perform_later).once
-    schedule_exports
+  context 'when automed export enabled' do
+    before do
+      account_with_yalty_access.available_modules.add(id: 'automatedexport')
+      account_with_yalty_access.save
+    end
+
+    it 'schedules export only once' do
+      expect(export_job).to receive(:perform_later).once
+      schedule_exports
+    end
+
+    it 'schedules export only for accounts with yalty special access and module enabled' do
+      expect(export_job).to receive(:perform_later).with(account_with_yalty_access)
+      schedule_exports
+    end
   end
 
-  it 'schedules export only for accounts with yalty special access' do
-    expect(export_job).to receive(:perform_later).with(account_with_yalty_access)
-    schedule_exports
+  context 'when automated export does not enabled' do
+    it 'do not schedule is yalty access enabled but module not' do
+      expect(export_job).to_not receive(:perform_later)
+      schedule_exports
+    end
   end
 
   it 'do not schedule job if SFTP is not configured' do
