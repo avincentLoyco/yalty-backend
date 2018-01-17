b9cf8c2c68a14ba81ed8d1227988f0159dc556f3
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2018-01-17 17:24:49 +0100

    Remove migration that creates unique keys

--- db/migrate/20180115085022_add_unique_index_on_time_off_policy_amount.rb
+++
@@ -1,5 +0,0 @@
-class AddUniqueIndexOnTimeOffPolicyAmount < ActiveRecord::Migration
-  def change
-    add_index :time_off_policies, [:amount, :time_off_category_id], unique: true, where: "(active IS TRUE AND reset IS FALSE)"
-  end
-end
