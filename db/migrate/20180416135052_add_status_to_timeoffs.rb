d8d12da0ca7dd314827c9e43d2fb5a897bc86f46
Roman Lemekha <roman.lemekha@monterail.com>
2018-07-20 10:47:02 +0200

    Fix migrations BDR postgres

--- db/migrate/20180416135052_add_status_to_timeoffs.rb
+++ db/migrate/20180416135052_add_status_to_timeoffs.rb
@@ -1,6 +1,8 @@
 class AddStatusToTimeoffs < ActiveRecord::Migration
   def change
-    add_column :time_offs, :approval_status, :integer, default: 0
+    add_column :time_offs, :approval_status, :integer
+    change_column_default :time_offs, :approval_status, 0
+    execute("UPDATE time_offs SET approval_status = 0")
     add_index :time_offs, :approval_status
   end
 end
