9c1e39f92a70205fe519c565c369e9b850fb3a69
Michał Januszkiewicz <michal.januszkiewicz@monterail.com>
2018-09-10 13:27:46 +0200

    [YA-1991] Sort unread notifications by created_at

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,5 +2,5 @@ class Notification < ActiveRecord::Base
   belongs_to :user, required: true, class_name: "Account::User", inverse_of: :notifications
   belongs_to :resource, polymorphic: true
 
-  scope :unread, -> { where(seen: false) }
+  scope :unread, -> { where(seen: false).order(created_at: :desc) }
 end
